597dc41c4d7133373c68b7393fe77789df519b47
PillowSoPaw <amadorable.angelo@gmail.com>
2015-05-29 10:39:53 +0800

    removed test comment since it was just a test

--- app/src/main/java/com/example/interns/listviewthing/listLocations.java
+++ app/src/main/java/com/example/interns/listviewthing/listLocations.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class listLocations extends ActionBarActivity {
-//test hihihihi
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
