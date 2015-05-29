64c25a2e5cb11c854c5fe38a6d17d0dfd3bc5827
PillowSoPaw <amadorable.angelo@gmail.com>
2015-05-29 15:36:35 +0800

    Initial List. Not Clickable yet.

--- app/src/main/java/com/example/interns/listviewthing/listLocations.java
+++ app/src/main/java/com/example/interns/listviewthing/listLocations.java
@@ -1,17 +1,48 @@
 package com.example.interns.listviewthing;
 
+import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 public class listLocations extends ActionBarActivity {
+
+    private RecyclerView recyclerView;
+    private listLocations_Recyclerview_Adapter adapter;
+
+    String[]locations;
+   // String[] locations = {"ajkfa","adhaksjdhakjsdh","kdfhasjkdhaksjdh","askldhahkl","fuck me","fuck my life"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_locations);
+        locations = getResources().getStringArray(R.array.locations);
+        recyclerView = (RecyclerView) findViewById(R.id.listLocations);
+        adapter = new listLocations_Recyclerview_Adapter(this,getData());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    public List<location> getData(){
+        List<location> data = new ArrayList<>();
+        int icon = R.drawable.generic_building;
+
+        for(int i = 0; i<locations.length;i++){
+            location current = new location();
+            current.name = locations[i];
+            data.add(current);
+        }
+
+        return data;
     }
 
     @Override
---
+++ app/src/main/java/com/example/interns/listviewthing/location.java
@@ -0,0 +1,9 @@
+package com.example.interns.listviewthing;
+
+/**
+ * Created by Interns on 5/29/2015.
+ */
+public class location {
+    String name;
+    int icon;
+}
